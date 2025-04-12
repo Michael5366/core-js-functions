34933bb6540bec2ba3abd886640955ab6ab5902a
Michael5366 <qwentin536@gmail.com>
2025-04-12 17:27:38 +0300

    feat: add logger (Sat, Apr 12, 2025 05:27 PM)

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -184,8 +184,16 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const formattedArgs = args.map((arg) => JSON.stringify(arg)).join(',');
+    const start = `${func.name}(${formattedArgs}) starts`;
+    logFunc(start);
+    const result = func(...args);
+    const end = `${func.name}(${formattedArgs}) ends`;
+    logFunc(end);
+    return result;
+  };
 }
 
 /**
